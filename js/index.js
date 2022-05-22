d52fa889af1cf5a2df4a345f7adf28d9084fecce
shantiago <you@example.com>
2022-05-22 17:06:26 -0500

    Se Mejoró el estilo de la web y se le agregaron algunos efectos

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+  $('.Presentacion').bxSlider({
+    mode: 'fade',
+  });
+  $('.Generos').bxSlider({
+    mode: 'fade',
+  });
+  $('.Regaeton').bxSlider({
+    mode: 'fade',
+  });
+  $('.Electronica').bxSlider({
+    mode: 'fade',
+  });
+});
